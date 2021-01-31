a9bc34491f548b3d09b0541efb50f367b8310081
Madeline Dale <mdale@ebsco.com>
2021-01-31 14:41:13 -0500

    connect to local db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,35 @@
 const { ApolloServer } = require("apollo-server");
+const { PrismaClient } = require("@prisma/client");
+const fs = require("fs");
+const path = require("path");
 
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`;
+const prisma = new PrismaClient();
 
 const resolvers = {
   Query: {
     info: () => `This is the API of the NFL Pickem League Website`,
+    players: async (parent, args, context) => {
+      return context.prisma.player.findMany();
+    },
+  },
+  Mutation: {
+    post: (parent, args, context) => {
+      const newPlayer = context.prisma.player.create({
+        data: {
+          name: args.name,
+        },
+      });
+      return newPlayer;
+    },
   },
 };
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
+  context: {
+    prisma,
+  },
 });
 
 server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
