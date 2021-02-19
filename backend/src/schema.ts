b72fad942d4834c196101b853e6a132cd3663954
Madeline Dale <mdale@ebsco.com>
2021-02-18 19:39:49 -0500

    create game

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -3,7 +3,30 @@ import { Context } from "./context";
 export const typeDefs = `
 type Query {
     teams: [Team!]!
+    players: [Player]
+    games: [Game]
+    picks: [Pick]
   }
+
+type Mutation {
+  createGame(data: GameCreateInput!): Game!
+}
+
+input GameCreateInput {
+  season: Int!
+  week: Int!
+  spread: Float
+  teams: TeamCreateManyWithoutTeamInput
+}
+
+input TeamCreateManyWithoutTeamInput{
+  create: [TeamCreateWithoutGameInput!]
+}
+
+input TeamCreateWithoutGameInput {
+  field: TeamField
+  teamId: Int!
+}
 
 type Player {
     id: ID!
@@ -53,6 +76,101 @@ export const resolvers = {
   Query: {
     teams: (_parent: any, _args: any, ctx: Context) => {
       return ctx.prisma.team.findMany({});
+    },
+    players: (_parent: any, _args: any, ctx: Context) => {
+      return ctx.prisma.player.findMany({});
+    },
+    games: (_parent: any, _args: any, ctx: Context) => {
+      return ctx.prisma.game.findMany({});
+    },
+    picks: (_parent: any, _args: any, ctx: Context) => {
+      return ctx.prisma.pick.findMany({});
+    },
+  },
+  Mutation: {
+    createGame: (_parent: any, args: any, ctx: Context) => {
+      return ctx.prisma.game.create(args);
+    },
+  },
+  Player: {
+    picks: (parent: any, _args: any, ctx: Context) => {
+      return ctx.prisma.player
+        .findUnique({
+          where: { id: parent.id },
+        })
+        .picks();
+    },
+  },
+  Pick: {
+    player: (parent: any, _args: any, ctx: Context) => {
+      return ctx.prisma.pick
+        .findUnique({
+          where: { id: parent.id },
+        })
+        .player();
+    },
+    game: (parent: any, _args: any, ctx: Context) => {
+      return ctx.prisma.pick
+        .findUnique({
+          where: { id: parent.id },
+        })
+        .game();
+    },
+    team: (parent: any, _args: any, ctx: Context) => {
+      return ctx.prisma.pick
+        .findUnique({
+          where: { id: parent.id },
+        })
+        .team();
+    },
+  },
+  Game: {
+    teams: (parent: any, _args: any, ctx: Context) => {
+      return ctx.prisma.game
+        .findUnique({
+          where: { id: parent.id },
+        })
+        .teams();
+    },
+    picks: (parent: any, _args: any, ctx: Context) => {
+      return ctx.prisma.game
+        .findUnique({
+          where: { id: parent.id },
+        })
+        .picks();
+    },
+    winner: (parent: any, _args: any, ctx: Context) => {
+      return ctx.prisma.game
+        .findUnique({
+          where: { id: parent.id },
+        })
+        .winner();
+    },
+  },
+  GameTeam: {
+    team: (parent: any, _args: any, ctx: Context) => {
+      return ctx.prisma.gameTeam
+        .findUnique({
+          where: {
+            teamId_gameId: {
+              teamId: parent.teamId,
+              gameId: parent.gameId,
+            },
+          },
+        })
+        .team();
+    },
+    game: (parent: any, _args: any, ctx: Context) => {
+      return ctx.prisma.gameTeam
+        .findUnique({
+          where: {
+            teamId_gameId: {
+              teamId: parent.teamId,
+              gameId: parent.gameId,
+            },
+          },
+        })
+        .game();
     },
   },
 };
--- backend/src/seed.ts
+++ backend/src/seed.ts
@@ -5,65 +5,296 @@ const prisma = new PrismaClient();
 
 // A `main` function so that we can use async/await
 async function main() {
-  await prisma.pick.deleteMany({});
-  await prisma.player.deleteMany({});
-  await prisma.gameTeam.deleteMany({});
-  await prisma.team.deleteMany({});
-  await prisma.game.deleteMany({});
+  // await prisma.pick.deleteMany({});
+  //await prisma.player.deleteMany({});
+  //await prisma.gameTeam.deleteMany({});
+  // await prisma.team.deleteMany({});
+  //await prisma.game.deleteMany({});
 
-  const madeline = await prisma.player.create({
-    data: {
-      name: "Madeline",
-    },
-  });
+  //const playersNames = ["Madeline", "Matthew", "Mike", "Sue", "David", "Eddie"];
+  // const footballTeams = [
+  //   {
+  //     city: "Arizona",
+  //     name: "Cardinals",
+  //     abr: "ARI",
+  //     conf: "NFC",
+  //     div: "West",
+  //   },
+  //   {
+  //     city: "Atlanta",
+  //     name: "Falcons",
+  //     abr: "ATL",
+  //     conf: "NFC",
+  //     div: "South",
+  //   },
+  //   {
+  //     city: "Baltimore",
+  //     name: "Ravens",
+  //     abr: "BAL",
+  //     conf: "AFC",
+  //     div: "North",
+  //   },
+  //   {
+  //     city: "Buffalo",
+  //     name: "Bills",
+  //     abr: "BUF",
+  //     conf: "AFC",
+  //     div: "EAST",
+  //   },
+  //   {
+  //     city: "Carolina",
+  //     name: "Panthers",
+  //     abr: "CAR",
+  //     conf: "NFC",
+  //     div: "South",
+  //   },
+  //   {
+  //     city: "Chicago",
+  //     name: "Bears",
+  //     abr: "CHI",
+  //     conf: "NFC",
+  //     div: "North",
+  //   },
+  //   {
+  //     city: "Cincinnati",
+  //     name: "Bengals",
+  //     abr: "CIN",
+  //     conf: "AFC",
+  //     div: "North",
+  //   },
+  //   {
+  //     city: "Cleveland",
+  //     name: "Browns",
+  //     abr: "CLE",
+  //     conf: "AFC",
+  //     div: "North",
+  //   },
+  //   {
+  //     city: "Dallas",
+  //     name: "Cowboys",
+  //     abr: "DAL",
+  //     conf: "NFC",
+  //     div: "East",
+  //   },
+  //   {
+  //     city: "Denver",
+  //     name: "Broncos",
+  //     abr: "DEN",
+  //     conf: "AFC",
+  //     div: "West",
+  //   },
+  //   {
+  //     city: "Detroit",
+  //     name: "Lions",
+  //     abr: "DET",
+  //     conf: "NFC",
+  //     div: "North",
+  //   },
+  //   {
+  //     city: "Green Bay",
+  //     name: "Packers",
+  //     abr: "GB",
+  //     conf: "NFC",
+  //     div: "North",
+  //   },
+  //   {
+  //     city: "Houston",
+  //     name: "Texans",
+  //     abr: "HOU",
+  //     conf: "AFC",
+  //     div: "South",
+  //   },
+  //   {
+  //     city: "Indianapolis",
+  //     name: "Colts",
+  //     abr: "IND",
+  //     conf: "AFC",
+  //     div: "South",
+  //   },
+  //   {
+  //     city: "Jacksonville",
+  //     name: "Jaquars",
+  //     abr: "JAX",
+  //     conf: "AFC",
+  //     div: "South",
+  //   },
+  //   {
+  //     city: "Kansas City",
+  //     name: "Chiefs",
+  //     abr: "KC",
+  //     conf: "AFC",
+  //     div: "West",
+  //   },
+  //   {
+  //     city: "Las Vegas",
+  //     name: "Raiders",
+  //     abr: "OAK",
+  //     conf: "AFC",
+  //     div: "West",
+  //   },
+  //   {
+  //     city: "Los Angeles",
+  //     name: "Chargers",
+  //     abr: "SD",
+  //     conf: "AFC",
+  //     div: "West",
+  //   },
+  //   {
+  //     city: "Los Angeles",
+  //     name: "Rams",
+  //     abr: "STL",
+  //     conf: "NFC",
+  //     div: "West",
+  //   },
+  //   {
+  //     city: "Miami",
+  //     name: "Dolphins",
+  //     abr: "MIA",
+  //     conf: "AFC",
+  //     div: "East",
+  //   },
+  //   {
+  //     city: "Minnesota",
+  //     name: "Vikings",
+  //     abr: "MIN",
+  //     conf: "AFC",
+  //     div: "North",
+  //   },
+  //   {
+  //     city: "New England",
+  //     name: "Patriots",
+  //     abr: "NE",
+  //     conf: "AFC",
+  //     div: "East",
+  //   },
+  //   {
+  //     city: "New Orleans",
+  //     name: "Saints",
+  //     abr: "NO",
+  //     conf: "NFC",
+  //     div: "South",
+  //   },
+  //   {
+  //     city: "New York",
+  //     name: "Giants",
+  //     abr: "NYG",
+  //     conf: "NFC",
+  //     div: "East",
+  //   },
+  //   {
+  //     city: "New York",
+  //     name: "Jets",
+  //     abr: "NYJ",
+  //     conf: "AFC",
+  //     div: "East",
+  //   },
+  //   {
+  //     city: "Philadelphia",
+  //     name: "Eagles",
+  //     abr: "PHI",
+  //     conf: "NFC",
+  //     div: "East",
+  //   },
+  //   {
+  //     city: "Pittsburgh",
+  //     name: "Steelers",
+  //     abr: "PIT",
+  //     conf: "AFC",
+  //     div: "North",
+  //   },
+  //   {
+  //     city: "San Francisco",
+  //     name: "49ers",
+  //     abr: "SF",
+  //     conf: "NFC",
+  //     div: "West",
+  //   },
+  //   {
+  //     city: "Seattle",
+  //     name: "Seahawks",
+  //     abr: "SEA",
+  //     conf: "NFC",
+  //     div: "West",
+  //   },
+  //   {
+  //     city: "Tampa Bay",
+  //     name: "Buccaneers",
+  //     abr: "TB",
+  //     conf: "NFC",
+  //     div: "South",
+  //   },
+  //   {
+  //     city: "Tennessee",
+  //     name: "Titans",
+  //     abr: "TEN",
+  //     conf: "AFC",
+  //     div: "South",
+  //   },
+  //   {
+  //     city: "Washington",
+  //     name: "Football Team",
+  //     abr: "WAS",
+  //     conf: "NFC",
+  //     div: "East",
+  //   },
+  // ];
 
-  console.log({ madeline });
+  // for (const name of playersNames) {
+  //   const player = await prisma.player.create({
+  //     data: {
+  //       name: name,
+  //     },
+  //   });
+  //   console.log({ player });
+  // }
 
-  const gameOne = await prisma.game.create({
-    data: {
-      season: 2018,
-      week: 1,
-      spread: -3.5,
-      teams: {
-        create: [
-          {
-            field: "AWAY",
-            team: {
-              create: {
-                city: "Denver",
-                name: "Broncos",
-              },
-            },
-          },
-          {
-            field: "HOME",
-            team: {
-              create: {
-                city: "Baltimore",
-                name: "Ravens",
-              },
-            },
-          },
-        ],
-      },
-    },
-    include: {
-      teams: true,
-    },
-  });
+  // footballTeams.forEach(async (team) => {
+  //   const t = await prisma.team.create({
+  //     data: {
+  //       name: team.name,
+  //       city: team.city,
+  //     },
+  //   });
+  //   console.log({ t });
+  // });
 
-  console.log({ gameOne });
+  // const gameOne = await prisma.game.create({
+  //   data: {
+  //     season: 2018,
+  //     week: 1,
+  //     spread: -3.5,
+  //   },
+  // });
+  // console.log({ gameOne });
+
+  // const gameTeamRelationshipHome = await prisma.gameTeam.create({
+  //   data: {
+  //     field: "HOME",
+  //     gameId: 5,
+  //     teamId: 9,
+  //   },
+  // });
+  // const gameTeamRelationshipAway = await prisma.gameTeam.create({
+  //   data: {
+  //     field: "AWAY",
+  //     gameId: 5,
+  //     teamId: 10,
+  //   },
+  // });
+
+  // console.log({ gameTeamRelationshipHome });
+  // console.log({ gameTeamRelationshipAway });
 
   const madelinePick = await prisma.pick.create({
     data: {
       player: {
-        connect: { name: "Madeline" },
+        connect: { id: 19 },
       },
       game: {
-        connect: { id: gameOne.id },
+        connect: { id: 5 },
       },
       team: {
-        connect: { name: "Broncos" },
+        connect: { id: 9 },
       },
     },
     include: {
