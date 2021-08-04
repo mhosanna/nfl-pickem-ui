39ac77777344aa84baaea60afa84c530a99b995a
Madeline Dale <mdale@ebsco.com>
2021-08-04 08:39:36 -0400

    make nav sticky

--- components/PageShell/PageShell.tsx
+++ components/PageShell/PageShell.tsx
@@ -3,29 +3,39 @@ import Navigation from "../Navigation";
 import Spacer from "../Spacer";
 
 const AppWrapper = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: 300px 1fr;
+  padding: 0px;
+  max-width: 100%;
+`;
+
+const NavOuterWrapper = styled.div`
+  position: relative;
+  z-index: 2;
 `;
 
 const NavWrapper = styled.div`
-  flex: 1;
-  height: 100vh;
   padding: 40px 0px;
   background-color: var(--background);
+  position: fixed;
+  top: 0px;
+  bottom: 0px;
 `;
 
 const InnerStyles = styled.div`
   flex: 3;
   height: 100vh;
-  margin: 0 auto;
   padding: 0px 5rem;
 `;
 
 export default function PageShell({ children }) {
   return (
     <AppWrapper>
-      <NavWrapper>
-        <Navigation />
-      </NavWrapper>
+      <NavOuterWrapper>
+        <NavWrapper>
+          <Navigation />
+        </NavWrapper>
+      </NavOuterWrapper>
       <InnerStyles>
         <Spacer size={48} />
         {children}
