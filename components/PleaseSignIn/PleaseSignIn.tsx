7332e77e04c2d3b0d73776e8c3b4c08d40d77a70
Madeline <mhosanna@gmail.com>
2021-09-03 07:45:46 -0400

    add request reset

--- components/PleaseSignIn/PleaseSignIn.tsx
+++ components/PleaseSignIn/PleaseSignIn.tsx
@@ -1,8 +1,22 @@
+import styled from "styled-components";
 import { usePlayer } from "../../lib/usePlayer";
+import RequestReset from "../RequestReset";
 import { SignIn } from "../SignIn";
 
 export default function PleaseSignIn({ children }) {
   const me = usePlayer();
-  if (!me) return <SignIn />;
+  if (!me)
+    return (
+      <GridStyles>
+        <SignIn />
+        <RequestReset />
+      </GridStyles>
+    );
   return children;
 }
+
+const GridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 2rem;
+`;
