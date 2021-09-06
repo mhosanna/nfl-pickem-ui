2943b112921bd51197d045aaf4218f120b64a61f
Madeline <mhosanna@gmail.com>
2021-09-06 15:44:49 -0400

    start 2021 season

--- config.ts
+++ config.ts
@@ -2,4 +2,4 @@
 // export const endpoint = `http://pickem-edge.ngrok.io/api/graphql`;
 export const endpoint = `http://localhost:3000/api/graphql`;
 export const prodEndpoint = `https://api.pick-em.club/api/graphql`;
-export const season = "2020";
+export const season = "2021";
